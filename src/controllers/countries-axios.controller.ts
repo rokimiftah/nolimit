1761ec024a52cd59e9ac8c680101dc31e6c2519c
Roki Miftah Kamaludin <rokimiftah@gmail.com>
2023-10-08 16:13:55 +0700

    feat: add receive country by code with axios Get country by code using Axios

--- src/controllers/countries-axios.controller.ts
+++ src/controllers/countries-axios.controller.ts
@@ -29,3 +29,42 @@ export const getAllCountriesWithAxios = async (req: Request, res: Response) => {
   }
 };
 
+// Receive country by code with Axios
+export const getCountryWithAxios = async (req: Request, res: Response) => {
+  try {
+    const code = req.params.code.toUpperCase();
+
+    const query = `
+    query getOneCountry($code: ID!){
+      country (code: $code) {
+        awsRegion
+        capital
+        code
+        currencies
+        currency
+        emoji
+        emojiU
+        name
+        native
+        phone
+        phones
+      }
+    }
+  `;
+
+    const response = await axios.post(
+      'https://countries.trevorblades.com/graphql',
+      {
+        query,
+        variables: { code }
+      }
+    );
+
+    const { data } = response.data;
+
+    res.json({ data });
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: 'An error occurred while fetching data' });
+  }
+};
